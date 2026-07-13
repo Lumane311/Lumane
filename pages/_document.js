a96847ba65dddca4d90da938d61a74eefd29a2eb
Lumane311 <calderonhernandezluisa@gmail.com>
2026-07-13 23:54:21 +0200

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,52 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="es">
+      <Head>
+        {/* ── Meta Pixel (LuMane) ───────────────────────────── */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1577970710353141');
+fbq('track', 'PageView');
+`,
+          }}
+        />
+        {/* ── Google Analytics (LuMane) ─────────────────────── */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-JB8S3WTHRB"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-JB8S3WTHRB');
+`,
+          }}
+        />
+      </Head>
+      <body>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1577970710353141&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
